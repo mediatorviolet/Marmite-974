b2af1a2019bdcc634d70fe249f942c316b6b68de
Dada97400 <humblot.david1@gmail.com>
2020-12-10 08:20:04 +0400

    heade.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php echo test ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marmite 974</title>
+</head>
+<body>
+    <?php include 'header.php'?>
+    <?php echo 'test' ?>
+</body>
+</html>
+
+
+
+
+
+
+
+
