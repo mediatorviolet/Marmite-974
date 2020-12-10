2c655eb1f8e70eee3e1f50157b4eb93605a5cc14
leo599simplon <leo599simplon@gmail.com>
2020-12-10 13:55:31 +0400

    ajout tableau de bord cuisinier HTML

---
+++ src/pages/tableau_cuisinier.php
@@ -0,0 +1,19 @@
+<?php include '../include/header.php'?>
+
+<div class="container-fluid ">
+  <h2 class="display-4 text-center p-lg-5 p-md-3 py-3">Tableau de bord</h2>
+  <table class=" table table-responsive table-hover table-dark">
+    <thead>
+      <tr>
+        <th class="align-middle text-center" scope="col">Titre</th>
+        <th class="align-middle text-center" scope="col">Date</th>
+        <th class="align-middle text-center" scope="col">Horaires</th>
+        <th class="align-middle text-center" scope="col">Durée</th>
+        <th class="align-middle text-center" scope="col">Effectif</th>
+        <th class="align-middle text-center" scope="col">Prix</th>
+        <th class="align-middle text-center" scope="col">Etat</th>
+        <th class="align-middle text-center" scope="col">Activer / Désactiver</th>
+        <th class="align-middle text-center" scope="col">Modifier</th>
+      </tr>
+    </thead>
+    <tbody>
