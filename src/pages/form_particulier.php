23d4d45370d6e3db95beb6da6cb2a479a7b04f5e
leo599simplon <leo599simplon@gmail.com>
2020-12-10 13:49:34 +0400

    suppression du lien css bootstrap dans les formulaires et ajout dans le header

--- src/pages/form_particulier.php
+++ src/pages/form_particulier.php
@@ -1,5 +1,3 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
 <?php include '../include/header.php'?>
 
 <div class="container d-flex justify-content-center mt-3  mb-3">
