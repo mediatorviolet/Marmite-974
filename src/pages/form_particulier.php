ed237a94a35a674add19e7b4e8666cde886028c4
leo599simplon <leo599simplon@gmail.com>
2020-12-11 15:27:46 +0400

    review fin de journée

--- src/pages/form_particulier.php
+++ src/pages/form_particulier.php
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 <?php include 'src/libs/fonctions/envoi_json.php'?>
-=======
 <?php include 'src/libs/fonctions/validation_form.php';
 global $class_alert;
 global $msg_alert ;?>
->>>>>>> 04b2c2e07b237a06a57017ea45625d5b5813f0f6
 
 <div class="container d-flex justify-content-center mt-3  mb-3">
 
@@ -72,11 +69,7 @@ global $msg_alert ;?>
         </div>
 
         <div class="col-sm-10 offset-2">
-<<<<<<< HEAD
     <button type="submit" action="form_particulier.php" name="Inscrire_Particulier" class="btn btn-primary px-4" >S'inscrire</button>
-=======
-    <button type="submit"  name="Inscrire_Particulier" class="btn btn-primary px-4" >S'inscrire</button>
->>>>>>> 04b2c2e07b237a06a57017ea45625d5b5813f0f6
   </div>
   <div class="form-group row">
                 <label for="id_particulier" class="col-md-3 col-form-label font-weight-bold"></label>
