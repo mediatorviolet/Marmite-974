71e67d9e4682ac7e2ad2454e5e44fb263df0af95
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-12-16 14:40:56 +0400

    w3c

--- src/pages/form_ajout.php
+++ src/pages/form_ajout.php
@@ -34,34 +34,33 @@ if (isset($_POST['Inscrire_Atelier'])) {
             <label for="description" class="form-label">Description</label>
         </div>
         <div class="col-md-6 form-floating mb-3">
-            <input type="date" class="form-control" name="date" placeholder="" required pattern="^?(0[1-9]|[12][0-9]|3[01])[\/](0[1-9]|1[012])[\/](19|20)\d\d$">
+            <input type="date" class="form-control" name="date" placeholder="" required>
             <label for="date" class="form-label">Date</label>
         </div>
         <div class="col-md-6 form-floating mb-3">
-            <input type="time" class="form-control" name="heure_debut" placeholder="" required pattern="/^(0?[1-9]|1[0-2]):[0-5][0-9]$/">
+            <input type="time" class="form-control" name="heure_debut" placeholder="" required>
             <label for="heure_debut" class="form-label">Heure de début</label>
         </div>
         <div class="col-md-6 form-floating mb-3">
-            <input type="number" class="form-control" name="duree" placeholder="" required pattern="[0-9]">
+            <input type="number" class="form-control" name="duree" placeholder="" required>
             <label for="duree" class="form-label">Durée</label>
         </div>
         <div class="col-md-6 form-floating mb-3">
-            <input type="number" class="form-control" name="effectif_max" placeholder="" required pattern = "[0-100]">
+            <input type="number" class="form-control" name="effectif_max" placeholder="" required>
             <label for="effectif_max" class="form-label">Effectif maximum</label>
         </div>
         <div class="col-md-6 form-floating mb-3">
-            <input type="number" class="form-control" name="prix" placeholder="" required pattern = "[0-10000]" min="0" max="10000" step="1">
+            <input type="number" class="form-control" name="prix" placeholder="" required min="0" max="10000" step="1">
             <label for="prix" class="form-label">Prix</label>
         </div>
         <div class="col-md-6 d-flex align-items-center mb-3">
             <label for="image" class="form-label me-3">Image</label>
-            <input type="file" class="form-control-lg" id="image" name="image" placeholder=""required >
+            <input type="file" class="form-control-lg" id="image" name="image" required >
         </div>
         <div class="col-12 d-flex justify-content-end">
             <button type="submit" name="Inscrire_Atelier" class="btn-lg btn-warning px-5">Ajouter</button>
         </div>
         <div class="form-group row">
-                <label for="id_atelier" class="col-md-3 col-form-label font-weight-bold"></label>
                 <div class="col-md-9">
                     <input type="hidden" class="form-control" id="id_atelier" name="id_atelier">
                 </div>
